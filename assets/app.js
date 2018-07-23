f83670cfcae6f0ba42312e263302a6ba9557c62f
Shelby Dix <shelbydix1@gmail.com>
2018-07-23 11:48:48 -0500

    Finished

---
+++ assets/app.js
@@ -0,0 +1,92 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCvDqdfxIPkqZgGn26_cDYG_ka90HMlP-o",
+    authDomain: "train-schedule-2d250.firebaseapp.com",
+    databaseURL: "https://train-schedule-2d250.firebaseio.com",
+    projectId: "train-schedule-2d250",
+    storageBucket: "",
+    messagingSenderId: "834594468834"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+
+$("#add-user").on("click", function (event) {
+    event.preventDefault();
+    $("#name-input").empty();
+    $("#destination-input").empty();
+    $("#time-input").empty();
+    $("#frequency-input").empty();
+
+
+
+    var trainName = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+    var firstTrainMoment = moment(firstTrain, "HH:mm");
+    var nowMoment = moment();
+  
+    var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, "minutes");
+    var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+    var minutesAway = frequency - minutesSinceLastArrival;
+  
+    var nextArrival = nowMoment.add(minutesAway, "minutes");
+    var formatNextArrival = nextArrival.format("HH:mm");
+
+    
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequency);
+    console.log(formatNextArrival);
+    console.log(minutesAway);
+
+
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        formatNextArrival: formatNextArrival,
+        minutesAway: minutesAway,
+    });
+
+});
+
+
+
+    database.ref().on("child_added", function (snapshot) {
+        
+        var tableItem = $("<tr>");
+        var row = $("<td>")
+
+
+        row = $("<td>" + snapshot.val().trainName + "</td>");
+        tableItem.append(row);
+
+        row = $("<td>" + snapshot.val().destination + "</td>");
+        tableItem.append(row);
+
+        row = $("<td>" + snapshot.val().frequency + "</td>");
+        tableItem.append(row);
+
+        row = $("<td>" + snapshot.val().formatNextArrival + "</td>")
+        tableItem.append(row);
+
+        row = $("<td>" + snapshot.val().minutesAway + "</td>")
+        tableItem.append(row);
+
+        $("#dataRow").append(tableItem);
+
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    
+
+
+    });
